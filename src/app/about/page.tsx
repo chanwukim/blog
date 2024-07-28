c46e4262b050a6e9bccde5c138bf6c2201a839dc
chanwukim <chnwu.kim@gmail.com>
2024-07-29 08:38:43 +0900

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,25 @@
+import { SITE_CONFIG } from "@/constants";
+
 export default function About() {
-  return <div></div>;
+  return (
+    <section>
+      <h1 className="text-3xl font-extrabold">ChanWu Kim</h1>
+
+      <p className="mt-8">
+        Full-stack web developer interested in UI/UX, currently focused on the frontend. <br />I
+        like writing code with an iced americano.
+      </p>
+
+      <div className="mt-4">
+        <a
+          target="_blank"
+          href={`https://github.com/${SITE_CONFIG.author.name}`}
+          rel="noopener noreferrer"
+          className="font-medium hover:underline"
+        >
+          Github
+        </a>
+      </div>
+    </section>
+  );
 }
