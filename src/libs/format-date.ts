1637066158884841aebc0cf82117ed753d844290
chanwukim <chnwu.kim@gmail.com>
2025-04-13 19:43:51 +0900

    날짜 형식화 함수 추가

---
+++ src/libs/format-date.ts
@@ -0,0 +1,9 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+};
