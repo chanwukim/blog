87cf19460408b49652d90afbbab9b2ae5e553154
chanwukim <chnwu.kim@gmail.com>
2025-04-20 22:19:47 +0900

    robots.ts 추가

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: "https://chanwu.dev/sitemap.xml",
+    host: "https://chanwu.dev",
+  };
+}
